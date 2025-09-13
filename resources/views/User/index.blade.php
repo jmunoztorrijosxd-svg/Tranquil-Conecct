3be5ef91fb447906844786792b76d2ea4d067947
Julian Muñoz <jmunoztorrijosxd@gmail.com>
2025-09-13 15:53:40 -0500

    Data tables agregadas

--- resources/views/User/index.blade.php
+++ resources/views/User/index.blade.php
@@ -4,22 +4,52 @@
             {{ __('Usuarios') }}
         </h2>
     </x-slot>
-
+    
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-6">
-                <h1 class="text-2xl font-bold text-gray-700">Lista de usuarios</h1>
-                <p class="mt-4 text-gray-600">Aquí puedes ver los usuarios registrados en el sistema.</p>
-                <ul>
-                @foreach ($users as $usuarios)
-                <li>{{ $usuarios->name }}</li>
-                @endforeach
-                
-                </ul>
-
-
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-4">
+                <table id="employees" class="display">
+                    <thead>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($users as $usuario)
+                            <tr>
+                                <td>{{ $usuario->name }}</td>
+                                <td>{{ $usuario->email }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
 </x-app-layout>
 
+{{-- jQuery + DataTables (CDN) --}}
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.8/css/jquery.dataTables.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.2/css/buttons.dataTables.min.css">
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.8/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.2/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.print.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
+
+<script>
+$(function() {
+    $('#employees').DataTable({
+        pageLength: 20,
+        dom: 'Bfrtip',
+        language: {
+            url: 'https://cdn.datatables.net/plug-ins/1.13.8/i18n/es-ES.json'
+        },
+        buttons: ['copy', 'csv', 'excel', 'pdf', 'print']
+    });
+});
+</script>
